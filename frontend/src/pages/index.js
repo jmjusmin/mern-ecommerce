eb09d6878402bfbe679112cbde679b8045e5a1aa
jmjusmin <jmjusmin1412@gmail.com>
2023-01-17 21:43:05 -0800

    Create routes

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as CartPage } from "./CartPage";
+export { default as HomePage } from "./HomePage";
+export { default as LoginPage } from "./LoginPage";
+export { default as ProductDetailsPage } from "./ProductDetailsPage";
+export { default as ProductListPage } from "./ProductListPage";
+export { default as RegisterPage } from "./RegisterPage";
